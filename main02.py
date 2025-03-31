bfc9c5ecd39672adfa5e71c9a8b7c348937e7115
joaosantanared <jvtreloso@gmail.com>
2025-03-31 14:52:32 -0300

    aumentopercentual Aula04

--- main02.py
+++ main02.py
@@ -1,7 +1,8 @@
 Nome = input("Seu nome")
 Idade = input("Sua idade")
-Salario = float(input("digite seu salario")
-
-
-
+Salario = float(input("digite seu salario"))
+PorcentagemAumento = float(input("Informe o acrescimo"))
+cash = (Salario * PorcentagemAumento) / 100
+SalarioAtualizado = Salario+cash
 print(f"Seu nome é {Nome}, sua idade é {Idade} e o seu salário é {Salario}")
+print(f"E seu aumento será {SalarioAtualizado}")
