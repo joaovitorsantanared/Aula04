4e1ce8effc3c3b36f3937e64656a832274620f2e
joaosantanared <jvtreloso@gmail.com>
2025-03-31 15:05:19 -0300

    quatrooperacoes Aula04

---
+++ main03.py
@@ -0,0 +1,14 @@
+Numero1 = float(input("Digite o primeiro numero"))
+Numero2 = float(input("Digite o segundo numero"))
+
+soma = Numero1 + Numero2
+print(f"O resultado da soma sera {soma}")
+
+subtracao = Numero1 - Numero2
+print(f"A subtracao sera {subtracao}")
+
+multiplicacao = Numero1 * Numero2
+print(f"A multiplicacao sera{multiplicacao}")
+
+divisao = Numero1/Numero2
+print(f"A divisao sera{divisao}")
